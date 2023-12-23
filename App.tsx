c3acb28151569133e4775c7ee23f870cc9705def
ISRAELFCOTA <israelf.cota1203@gmail.com>
2023-12-23 16:31:53 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text } from "react-native";
+
+const App: React.FunctionComponent = () => {
+  return(
+    <Text>Hello Word</Text>
+  );
+};
+
+export default App;
